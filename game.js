485fe2cbb6a3406d736f443f8602cbc5e0ed96c2
manpreetsingh04m <ms2404mk@gmail.com>
2023-10-20 09:13:26 +0530

    Working Game

--- game.js
+++ game.js
@@ -1,5 +1,73 @@
 // Iteration 2: Generate 2 random number and display it on the screen
+var num1;
+var num2;
 
+let num1_box = document.getElementById("number1");
+let num2_box = document.getElementById("number2");
+
+function generateRandomNumber() {
+  num1 = Math.floor(Math.random() * 100);
+  num2 = Math.floor(Math.random() * 100);
+  num1_box.textContent = num1;
+  num2_box.textContent = num2;
+}
+
+generateRandomNumber();
+
+function thing(){
+  score++;
+  localStorage.setItem("score",score);
+  generateRandomNumber();
+  clearInterval(interval);
+  startTimer();
+}
 // Iteration 3: Make the options button functional
 
+let greater_than = document.getElementById("greater-than");
+let equal_to = document.getElementById("equal-to");
+let lesser_than = document.getElementById("lesser-than");
+let score = 0;
+
+greater_than.onclick = () => {
+  if (num1 > num2) {
+    thing()
+  } else {
+    window.open("./gameover.html", "_self");
+  }
+};
+
+equal_to.onclick = () => {
+  if (num1 === num2) {
+    thing()
+  } else {
+    window.open("./gameover.html", "_self");
+  }
+};
+
+lesser_than.onclick = () => {
+  if (num2 > num1) {
+   
+    thing()
+  } else {
+    window.open("./gameover.html", "_self");
+  }
+};
+
 // Iteration 4: Build a timer for the game
+
+let time = 5;
+let time_box = document.getElementById("timer");
+let interval;
+function startTimer() {
+  time = 5;
+  interval = setInterval(() => {
+    if (time == -1) {
+      // clearInterval(interval);
+      window.open("./gameover.html", "_self");
+    }
+    time_box.innerHTML = time;
+    time--;
+  }, 1000);
+}
+startTimer();
+localStorage.setItem("score",score);
--- score.js
+++ score.js
@@ -1,1 +1,8 @@
 // Iteration 5: Store the player score and display it on the game over screen
+var play_again = document.getElementById("play-again-button");
+play_again.onclick = () => {
+  window.open("./game.html", "_self");
+};
+var score_board = document.getElementById("score-board");
+let value=localStorage.getItem("score");
+score_board.innerHTML = value;
